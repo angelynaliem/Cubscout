48218a122381ceca237f90092ade4a0959de94d8
angelynaliem <angelynaliem@gmail.com>
2022-02-05 01:00:16 -0500

    Updated styling

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //Query Selectors
 const burger = document.querySelector(".burger");
+const logo = document.querySelector(".cubscout-logo");
 
 //Event listeners
 burger.addEventListener("click", navToggle);
@@ -8,15 +9,17 @@ burger.addEventListener("click", navToggle);
 function navToggle(e) {
   if (!e.target.classList.contains("active")) {
     e.target.classList.add("active");
-    gsap.to(".line-one", 0.5, { rotate: "45", y: 5, background: "black" });
-    gsap.to(".line-two", 0.5, { rotate: "-45", y: -5, background: "black" });
+    gsap.to(".line-one", 0.5, { rotate: "45", y: 5, background: "white" });
+    gsap.to(".line-two", 0.5, { rotate: "-45", y: -5, background: "white" });
     gsap.to(".menu-bar", 1, { clipPath: "circle(2500px at 100% -10%" });
+    logo.style.display = "none";
     document.body.classList.add("hide");
   } else {
     e.target.classList.remove("active");
     gsap.to(".line-one", 0.5, { rotate: "0", y: 0, background: "black" });
     gsap.to(".line-two", 0.5, { rotate: "0", y: 0, background: "black" });
     gsap.to(".menu-bar", 1, { clipPath: "circle(50px at 100% -10%" });
+    logo.style.display = "block";
     document.body.classList.remove("hide");
   }
 }
